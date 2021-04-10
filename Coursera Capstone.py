36e3d4c6928357f624bd03399524212ead8ff4eb
Tasa92 <82259458+Tasa92@users.noreply.github.com>
2021-04-10 10:15:05 +0200

    Update Coursera Capstone.py

--- Coursera Capstone.py
+++ Coursera Capstone.py
@@ -3,18 +3,11 @@
 
 # # Capstone Project
 
-# ### This notebook will be mainly used for the capstone project of this course
-
-# In[1]:
-
+# ## This notebook will be mainly used for the capstone project of this course
 
 # Importing libraries
 import numpy as np
 import pandas as pd 
-
-
-# In[2]:
-
 
 # First task
 print("Hello Capstone Project Course!")
