12837f75a0d8c61d1e058be6bb6ed92c547b1780
Tasa92 <82259458+Tasa92@users.noreply.github.com>
2021-04-10 10:13:28 +0200

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# ### This notebook will be mainly used for the capstone project of this course
+
+# In[1]:
+
+
+# Importing libraries
+import numpy as np
+import pandas as pd 
+
+
+# In[2]:
+
+
+# First task
+print("Hello Capstone Project Course!")
+
